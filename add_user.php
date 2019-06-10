a133d6ba2aa5d2ab9b9aa54ec4215c3edb783318
Victor <vicsmaloba@gmail.com>
2019-06-10 16:12:50 +0300

    password hashing

--- add_user.php
+++ add_user.php
@@ -2,20 +2,29 @@
 
 $con=new mysqli("us-cdbr-iron-east-02.cleardb.net", "b54168242dce39", "2ac0997a", "heroku_fb7865bc2c3f5fc");
 
+$mobile=$_POST['mobile'];
+$password=$_POST['password'];
+$name=$_POST['name'];
+$address=$_POST['address'];
+$ac=bin2hex(openssl_random_pseudo_bytes(64));
+$salted="AHB524bave".$password."038HDSBpmcn";
+$hashed_password=hash('sha512',$salted);
+
 $st_check=$con->prepare("select * from users where mobile = ?");
-$st_check->bind_param("s", $_GET["mobile"]);
+$st_check->bind_param("s", $mobile);
 $st_check->execute();
 $rs=$st_check->get_result();
 
 if ($rs->num_rows==0)
 {
 
-$st=$con->prepare("insert into users values(?,?,?,?)");
-$st->bind_param("ssss", $_GET["mobile"],$_GET["password"], $_GET["name"], $_GET["address"]);
+$st=$con->prepare("insert into users values(?,?,?,?,?)");
+$st->bind_param("sssss", $mobile,$hashed_password, $name, $address, $ac);
 $st->execute();
-
 echo "1";
 }
+
+
 else 
     echo "0";
 
--- login.php
+++ login.php
@@ -2,14 +2,24 @@
 
 $con=new mysqli("us-cdbr-iron-east-02.cleardb.net", "b54168242dce39", "2ac0997a", "heroku_fb7865bc2c3f5fc");
 
-$st_check=$con->prepare("select * from users where mobile = ? and password = ?");
-$st_check->bind_param("ss", $_GET["mobile"], $_GET["password"]);
+$mobile=$_POST['mobile'];
+$password=$_POST['password'];
+$salted="AHB524bave".$password."038HDSBpmcn";
+$hashed_password= hash('sha512',$salted);
+
+$st_check=$con->prepare("select * from users where mobile ='$mobile' and password ='$hashed_password'");
+//$st_check->bind_param("ss", $mobile,$password);
 $st_check->execute();
 $rs=$st_check->get_result();
 
-if ($rs->num_rows==0)
-    echo "0";
+if ($rs->num_rows!=0) 
+{
+   $act = bin2hex(openssl_random_pseudo_bytes(64));
+	$st=$con->prepare("update users set access_token=? where mobile = ?");
+	$st->bind_param("ss",$act, $mobile);
+	$st->execute();
+	echo $act;
+}
 else 
-    echo "1";
-    
+    echo "0";
 
