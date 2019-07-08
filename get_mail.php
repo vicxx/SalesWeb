f0eb484c5585de7f11341286bb531b6f6abb39be
Victor <vicsmaloba@gmail.com>
2019-07-08 17:34:23 +0300

    get mail correction

--- get_mail.php
+++ get_mail.php
@@ -3,8 +3,8 @@
 session_start();
 
 $con=new mysqli("us-cdbr-iron-east-02.cleardb.net", "b54168242dce39", "2ac0997a", "heroku_fb7865bc2c3f5fc");
-$st=$con->prepare("select name from users where email= ?");
-$st->bind_param("s", $_GET["email"]);
+$st=$con->prepare("select email from users where mobile= ?");
+$st->bind_param("s", $_GET["mobile"]);
 $st->execute();
 $rs=$st->get_result();
 $row=$rs->fetch_assoc();
